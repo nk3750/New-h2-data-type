b23ecdbd25e84dacfe422ac2890868b9fb8ecb69
Neelabh <nk3750@rit.edu>
2019-10-23 19:51:41 -0400

    work till now

--- House.java
+++ House.java
@@ -18,11 +18,11 @@ public class House implements Serializable {
                     "Barndominium","Mansion","Yurt","Castle","Palace",
                     "Chateau","Villa","Manor","Fort","Cave"));
 
-    private String house;
+    public String housename;
 
 
-    public House(String house) throws SQLException{
-        checkValidity(house);
+    public House(String housename) throws SQLException{
+        checkValidity(housename);
     }
     @Override
     public boolean equals(Object o){
@@ -33,7 +33,7 @@ public class House implements Serializable {
             return false;
         }
         House h = (House) o;
-        if (house.equals(h.house)){
+        if (housename.equals(h.housename)){
             return true;
         }
         return false;
@@ -43,12 +43,12 @@ public class House implements Serializable {
     public int hashCode(){
         //return the index of house from the list, the indexes are uniques
         // and there aren't any duplicates.
-         return houseList.indexOf(house);
+         return houseList.indexOf(housename);
     }
 
     @Override
     public String toString(){
-        return house;
+        return housename;
     }
 
     public void checkValidity(String house) throws SQLException{
---
+++ valueHouse.java
@@ -0,0 +1,84 @@
+package org.h2.value;
+
+import org.h2.engine.CastDataProvider;
+import org.h2.util.JdbcUtils;
+import org.h2.util.StringUtils;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class valueHouse extends Value {
+
+    protected House house;
+
+
+    public valueHouse(String houseName) throws SQLException {
+        this.house=new House(houseName);
+    }
+
+    public valueHouse(House h){
+        this.house=h;
+    }
+
+    public static valueHouse get(House h){
+        valueHouse valueHouseObject = new valueHouse(h);
+        return valueHouseObject;
+
+    }
+//    @Override
+//    public String getSQL(){
+//        return house.toString();
+//    }
+
+    @Override
+    public TypeInfo getType(){
+        return TypeInfo.TYPE_HOUSE;
+    }
+
+    @Override
+    public StringBuilder getSQL(StringBuilder builder) {
+        return StringUtils.quoteStringSQL(builder, house.toString());
+    }
+
+    @Override
+    public int getValueType() {
+        return HOUSE;
+    }
+
+    @Override
+    public String getString() {
+        return house.toString();
+    }
+
+    @Override
+    public Object getObject() {
+        return house;
+    }
+
+    @Override
+    public void set(PreparedStatement prep, int parameterIndex) throws SQLException {
+        Object obj = JdbcUtils.deserialize(getBytesNoCopy(), getDataHandler());
+        prep.setObject(parameterIndex, obj, Types.JAVA_OBJECT);
+    }
+
+    @Override
+    public int hashCode() {
+        return house.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if ((other==null) || !(other instanceof valueHouse)){
+            return false;
+        }
+        return house.equals((valueHouse)other);
+    }
+
+    @Override
+    public int compareTypeSafe(Value o, CompareMode mode, CastDataProvider provider) {
+        valueHouse vHouse = (valueHouse) o.getObject();
+        return mode.compareString(this.house.toString(),vHouse.house.toString()
+                ,false);
+    }
+}
